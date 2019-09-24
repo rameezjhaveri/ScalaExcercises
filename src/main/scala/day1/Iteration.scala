1fad5b8ce5c1186fafb0ff7300d496429f3cae4e
rameezjhaveri <rameezjhaveri94@gmail.com>
2019-09-24 15:27:42 +0100

    Iteration - fixed

--- src/main/scala/day1/Iteration.scala
+++ src/main/scala/day1/Iteration.scala
@@ -1,7 +1,11 @@
 package day1
 
 object Iteration extends App {
-def multiplePrints(text:String,x:Int):Unit=for(x<-1 to x) println(text)
+val start = 1
+  def multiplePrints(text:String,x:Int):Unit= {
+  if(x>start) multiplePrints(text, x-1)
+    println(text)
+  }
 
   multiplePrints("Hi", 3)
 }

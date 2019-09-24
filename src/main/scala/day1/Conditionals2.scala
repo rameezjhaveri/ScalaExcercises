6aaf6c4d4459c3d128f7705a5284804d7a53b4da
rameezjhaveri <rameezjhaveri94@gmail.com>
2019-09-24 13:34:48 +0100

    Iteration

--- src/main/scala/day1/Conditionals2.scala
+++ src/main/scala/day1/Conditionals2.scala
@@ -3,5 +3,5 @@ package day1
 object Conditionals2 extends App{
   def sumOf(value1:Int,value2:Int,value3:Boolean):Int=
     if(value1== 0) value2 else if(value2== 0) value1 else if (value3==true) value1+value2 else value1*value2
-  println(sumOf(10,3, true))
+  println(sumOf(0,0, false))
 }
---
+++ src/main/scala/day1/Iteration.scala
@@ -0,0 +1,7 @@
+package day1
+
+object Iteration extends App {
+def multiplePrints(text:String,x:Int):Unit=for(x<-1 to x) println(text)
+
+  multiplePrints("Hi", 3)
+}

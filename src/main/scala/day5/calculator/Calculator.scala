fbc679aed9364baddd898425995e0562ca52a7f2
rameezjhaveri <rameezjhaveri94@gmail.com>
2019-09-30 13:22:02 +0100

    ScalaTesting Calculator test5 - passed

--- src/main/scala/day5/calculator/Calculator.scala
+++ src/main/scala/day5/calculator/Calculator.scala
@@ -4,5 +4,6 @@ object Calculator extends App {
   def sum(number1:Int, number2:Int, operator:String):Any= (operator) match{
     case "+" => number1 + number2
     case "-" => number1 - number2
+    case "*" => number1 * number2
   }
 }

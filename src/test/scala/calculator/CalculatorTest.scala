5800f2daa83ee1a994c7386dca6c9f122af23021
rameezjhaveri <rameezjhaveri94@gmail.com>
2019-09-30 13:03:14 +0100

    ScalaTesting Calculator test2 - passed

--- src/test/scala/calculator/CalculatorTest.scala
+++ src/test/scala/calculator/CalculatorTest.scala
@@ -3,18 +3,15 @@ package calculator
 import resources.BaseTestingClass
 
 class CalculatorTest extends BaseTestingClass {
-def calculator():Any=2
+def calculator(number1:Int):Any=number1*2
 
 
   test("1+1 should return 2"){
-    assert(calculator()==2)
+    assert(calculator(1)==2)
   }
 
   test("2+2 should return 4"){
-    assert(calculator()==4)
+    assert(calculator(2)==4)
   }
-
-
-
 }
 
